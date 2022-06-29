9eef46f5d3dd886acb48eb08d14a63e322bd12d4
Youcef <youcefboutrig92@gmail.com>
2022-06-29 16:20:20 +0200

    Add a middleware for header authentification

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { gql } from "graphql-request";
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+const PROFILE_DATA = gql`
+  query GetProfile {
+    getProfile {
+      id
+      username
+      email
+    }
+  }
+`;
 
 const ProfileScreen = (): JSX.Element => {
-  const profileDataQuery = gql`
-    query GetProfile {
-      getProfile {
-        id
-        username
-        email
-      }
-    }
-  `;
-
+  const { data, loading, error } = useQuery(PROFILE_DATA);
+  useEffect(() => {});
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.userInfo}>Profile</Text>
+        {loading ? (
+          <Text style={styles.userInfo}>Loading ...</Text>
+        ) : (
+          <View>
+            <Text style={styles.userInfo}>Profile</Text>
+            <Text style={styles.userInfo}>
+              Username : {data.getProfile.username}
+            </Text>
+            <Text style={styles.userInfo}>
+              E-mail : {data.getProfile.email}
+            </Text>
+          </View>
+        )}
       </View>
     </>
   );
